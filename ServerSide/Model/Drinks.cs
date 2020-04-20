f32875b47af9ab4cc626f0c281390f416a9be76d
Sneppi Damsgård <seppihd@hotmail.com>
2020-04-20 13:07:14 +0200

    Added classes for Persons and Drinks

---
+++ ServerSide/Model/Drinks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ServerSide.Model
+{
+    class Drinks
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ]+$0-9\-")]
+        public string Name { get; set; }
+
+        public int StandardDrinks { get; set; }
+
+        public int TimesDrank { get; set; }
+    }
+}
---
+++ ServerSide/Model/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ServerSide.Model
+{
+    class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ]+$\-", ErrorMessage = "Only letters are allowed {0}")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ]+$\-", ErrorMessage = "Only letters are allowed {0}")]
+        public string LastName { get; set; }
+
+        [Required]
+        public bool Gender { get; set; }
+
+        [Required]
+        [Range(40, 200, ErrorMessage = "You weight somewhere between 40 and 200, type in your real weight.")]
+        public int weight { get; set; }
+
+        public float CurPromille { get; set; }
+
+        public float TopPromille { get; set; }
+
+        public int RecommendedWater { get; set; }
+
+        [Required]
+        public DateTime DrinkingStart { get; set; }
+    }
+}
