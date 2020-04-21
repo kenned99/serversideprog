b7faff4953b99da323e7f76225339755c98c4008
Kenneth Jensen <kenneth_jensen_99@hotmail.com>
2020-04-21 13:18:25 +0200

    added people enum

---
+++ WebApplication1/Pages/PersonList.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServerSide;
+using ServerSide.Model;
+
+namespace WebApplication1.Pages
+{
+    public class PersonListModel : PageModel
+    {
+        private readonly IServersideAccess serverside;
+
+        public PersonListModel(IServersideAccess serverside)
+        {
+            this.serverside = serverside;
+        }
+        public IEnumerable<Person> People => serverside.GetPersonByName("").OrderBy(x => x.Id);
+        public void OnGet()
+        {
+
+        }
+
+    }
+}
