919178aab53f26f32f299fd52ac9455e0a76c8f0
Sneppi Damsgård <seppihd@hotmail.com>
2020-04-21 09:47:14 +0200

    Added "Stats" - Crashes

---
+++ WebApplication1/Pages/Stats.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServerSide;
+using ServerSide.Model;
+
+namespace WebApplication1.Pages
+{
+    public class StatsModel : PageModel
+    {
+        private readonly IServersideAccess ServersideAccess;
+
+        public StatsModel(IServersideAccess SA)
+        {
+            this.ServersideAccess = SA;
+        }
+
+        public Person Person { get; set; }
+
+        public void OnGet(int PersonId)
+        {
+            Person = ServersideAccess.GetPerson(PersonId);
+        }
+    }
+}
