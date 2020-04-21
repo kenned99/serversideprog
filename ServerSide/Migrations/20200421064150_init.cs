6a8d647339241c9c912d09de3b5307ad02373797
Sebastian <ostehandler@hotmail.dk>
2020-04-21 10:16:23 +0200

    db satup nogenlunde

---
+++ ServerSide/Migrations/20200421064150_init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServerSide.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 60, nullable: false),
+                    LastName = table.Column<string>(maxLength: 60, nullable: false),
+                    Gender = table.Column<bool>(nullable: false),
+                    Weight = table.Column<int>(nullable: false),
+                    CurPromille = table.Column<float>(nullable: false),
+                    TopPromille = table.Column<float>(nullable: false),
+                    RecommendedWater = table.Column<int>(nullable: false),
+                    DrinkingStart = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
